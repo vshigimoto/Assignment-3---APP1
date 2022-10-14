6200d3f48b0276c8fff2fc9182c0b79eb5b89eaa
vshigimoto <96425010+vshigimoto@users.noreply.github.com>
2022-10-15 03:00:59 +0600

    Delete ass3 directory

--- ass3/req.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://solana-gateway.moralis.io/nft/mainnet/8RDo11QJ6CL5EfQeitLCRToC8xWafFKuu8HDzP2rCLWm%2F/metadata"
-
-headers = {"accept": "application/json"}
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
