89f5400b4cf41fc250cbc276d350fc3c8ab41131
vshigimoto <shigimoto@mail.ru>
2022-10-16 14:43:15 +0600

    update test

--- test/req.py
+++ test/req.py
@@ -1,9 +1,14 @@
 import requests
 
-url = "https://solana-gateway.moralis.io/nft/mainnet/8RDo11QJ6CL5EfQeitLCRToC8xWafFKuu8HDzP2rCLWm%2F/metadata"
+url = "https://solana-gateway.moralis.io/nft/mainnet/Ek64ZYUJ8oaDwSAhumzP8VzXyaE1ZW2xfcaufDXHyCY4/metadata"
 
-headers = {"accept": "application/json"}
+headers = {
+    "accept": "application/json",
+    "X-API-Key": "F5GCxxgXhLDTENlIDl8RHLBva7jXUC0SQuyrIV8jL1LJ8H9ZznttU81G50Rrhwqq"
+}
 
 response = requests.get(url, headers=headers)
-
+payload = response.json()
+uri = payload["symbol"]
 print(response.text)
+print(uri)
